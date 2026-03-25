97d4ebd8c33a6e951c12f4ca068d89fbd3e515e7
kikuen <hap0p9y@nate.com>
2026-03-25 10:57:12 +0900

    sample project 정리

--- src/main/java/net/koreate/team/HomeController.java
+++ src/main/java/net/koreate/team/HomeController.java
@@ -1,14 +1,7 @@
 package net.koreate.team;
 
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-=======
-import org.springframework.web.bind.annotation.GetMapping;
->>>>>>> branch 'master' of https://github.com/kikuen/SpringGitTeamTest2026.git
 
 @Controller
 public class HomeController {
